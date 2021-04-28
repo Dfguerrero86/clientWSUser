0391c92362e61be0ebd833403a6c0d1dbed1606d
Danny Guerrero <dguerrero@redesis.com>
2021-04-27 21:43:30 -0500

    Versión inicial

---
+++ Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace clientWSUser
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            wsUser.Service1Client oClient = new wsUser.Service1Client();
+            String res = oClient.getApellidoPersona("1085254773");
+            MessageBox.Show(res.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
