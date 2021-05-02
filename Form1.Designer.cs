d3aa5ff56892b26093830be1453b5f44049d7bee
Danny Guerrero <dguerrero@redesis.com>
2021-05-02 00:40:03 -0500

    Se incluye lógica en vista para crud final

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -30,28 +30,19 @@ namespace clientWSUser
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button1 = new System.Windows.Forms.Button();
             this.dataGridView = new System.Windows.Forms.DataGridView();
-            this.personaBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.tipoDocumentoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.numeroDocumentoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.primerNombreDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.primerApellidoDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.DetalleBtn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.ActualizarBtn = new System.Windows.Forms.DataGridViewButtonColumn();
             this.EliminarBtn = new System.Windows.Forms.DataGridViewButtonColumn();
+            this.personaBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.personaBindingSource)).BeginInit();
             this.SuspendLayout();
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(661, 22);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // dataGridView
             // 
@@ -65,18 +56,15 @@ namespace clientWSUser
             this.primerNombreDataGridViewTextBoxColumn,
             this.primerApellidoDataGridViewTextBoxColumn,
             this.DetalleBtn,
+            this.ActualizarBtn,
             this.EliminarBtn});
             this.dataGridView.DataSource = this.personaBindingSource;
-            this.dataGridView.Location = new System.Drawing.Point(12, 85);
+            this.dataGridView.Location = new System.Drawing.Point(12, 46);
             this.dataGridView.Name = "dataGridView";
             this.dataGridView.ReadOnly = true;
-            this.dataGridView.Size = new System.Drawing.Size(603, 150);
+            this.dataGridView.Size = new System.Drawing.Size(684, 249);
             this.dataGridView.TabIndex = 1;
             this.dataGridView.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView_CellContentClick);
-            // 
-            // personaBindingSource
-            // 
-            this.personaBindingSource.DataSource = typeof(clientWSUser.wsUser.Persona);
             // 
             // tipoDocumentoDataGridViewTextBoxColumn
             // 
@@ -115,6 +103,15 @@ namespace clientWSUser
             this.DetalleBtn.UseColumnTextForButtonValue = true;
             this.DetalleBtn.Width = 80;
             // 
+            // ActualizarBtn
+            // 
+            this.ActualizarBtn.HeaderText = "";
+            this.ActualizarBtn.Name = "ActualizarBtn";
+            this.ActualizarBtn.ReadOnly = true;
+            this.ActualizarBtn.Text = "Actualizar";
+            this.ActualizarBtn.UseColumnTextForButtonValue = true;
+            this.ActualizarBtn.Width = 80;
+            // 
             // EliminarBtn
             // 
             this.EliminarBtn.HeaderText = "";
@@ -124,16 +121,30 @@ namespace clientWSUser
             this.EliminarBtn.UseColumnTextForButtonValue = true;
             this.EliminarBtn.Width = 80;
             // 
+            // personaBindingSource
+            // 
+            this.personaBindingSource.DataSource = typeof(clientWSUser.wsUser.Persona);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(588, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Agregar Persona";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoValidate = System.Windows.Forms.AutoValidate.EnablePreventFocusChange;
-            this.ClientSize = new System.Drawing.Size(825, 395);
+            this.ClientSize = new System.Drawing.Size(707, 307);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.dataGridView);
-            this.Controls.Add(this.button1);
             this.Name = "Form1";
-            this.Text = "Administración de Persona";
+            this.Text = "Personas";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.personaBindingSource)).EndInit();
@@ -142,15 +153,15 @@ namespace clientWSUser
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
         private System.Windows.Forms.DataGridView dataGridView;
         private System.Windows.Forms.BindingSource personaBindingSource;
+        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.DataGridViewTextBoxColumn tipoDocumentoDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn numeroDocumentoDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn primerNombreDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewTextBoxColumn primerApellidoDataGridViewTextBoxColumn;
         private System.Windows.Forms.DataGridViewButtonColumn DetalleBtn;
+        private System.Windows.Forms.DataGridViewButtonColumn ActualizarBtn;
         private System.Windows.Forms.DataGridViewButtonColumn EliminarBtn;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -10,40 +10,36 @@ namespace clientWSUser
             InitializeComponent();
         }
 
-        private void button1_Click(object sender, EventArgs e)
-        {
-            wsUser.Service1Client oClient = new wsUser.Service1Client();
-            String res = oClient.getApellidoPersona("1085254773");
-            MessageBox.Show(res.ToString());
-        }
-
         private void Form1_Load(object sender, EventArgs e)
         {
             wsUser.Service1Client oClient = new wsUser.Service1Client();
             dataGridView.DataSource = oClient.getPersonas();
-            //dataGridView.Columns[5].
-
-
         }
 
         private void dataGridView_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.ColumnIndex == 4) {
-                wsUser.Service1Client oClient = new wsUser.Service1Client();
-                wsUser.Persona persona = oClient.getPersona(dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString());
-                MessageBox.Show("Tipo Documento: " + persona.TipoDocumento + "\nNúmero Documento: " + persona.NumeroDocumento
-                    + "\nPrimer Nombre: " + persona.PrimerNombre + "\nSegundo Nombre: " + persona.SegundoNombre
-                    + "\nPrimer Apellido: " + persona.PrimerApellido + "\nSegundo Apellido: " + persona.SegundoApellido
-                    + "\nGrupo Sanguineo: " + persona.GrupoSanguineo + "\nSexo: " + persona.Sexo
-                    + "\nDirección: " + persona.Direccion + "\nCelular: " + persona.Celular);
+                var form2 = new Form2(dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString(), false);
+                form2.Show();
             }
             if (e.ColumnIndex == 5)
+            {
+                var form2 = new Form2(dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString(), true);
+                form2.Show();
+            }
+            if (e.ColumnIndex == 6)
             {
                 wsUser.Service1Client oClient = new wsUser.Service1Client();
                 Int64 res=oClient.deletePersona(dataGridView.Rows[e.RowIndex].Cells[1].Value.ToString());
                 MessageBox.Show(res==1?"Eliminado":"No Eliminado");
                 dataGridView.DataSource = oClient.getPersonas();
             }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            var form2 = new Form2(null, true);
+            form2.Show();
         }
     }
 }
